19de528ab4eaa1f9bafdef6d1bd8abcc44c73fa8
Keith T. Star <keith@uberfoo.com>
2016-03-30 19:21:57 -0700

    Added initial go at creating a Python VirtualEnv for development and execution.

--- prototype/README.md
+++ prototype/README.md
@@ -6,9 +6,10 @@ First off, you'll need Python 3.4.3.  Python 3.4.x may work just as well, but I 
 
 The process is anything but automagic, and we hope to fix that before too much longer.
 
-### Generate JSON schema from PDBx/mmCIF star file
-7. `cd <sphinx_repo>/prototype`
-7. `./bootstrap.py`
+### Install Python Virtual Environment, install dependencies and generate JSON schema from PDBx/mmCIF star file
+1. `cd <sphinx_repo>/prototype`
+2. `python3 bootstrap.py`
+3. `source env/bin/activate`
 
 ### Build the Geometric Flow plugin
 7. `cd <sphinx_repo>/prototype/plugins/Geoflow/src`
@@ -17,6 +18,7 @@ The process is anything but automagic, and we hope to fix that before too much l
 7. `cmake ..`
 7. `make`
 7. `cp geoflow.so ../..`
+*Check the README file in the Geoflow/src directory if building on Windows or OSX*
 
 ## Testing the Beast
 We are currently using Nose to run the unit tests.  From the `prototype` directory just run `nosetests`.  Note that you'll need the appropriate version for Python 3.  In my case (OS X using homebrew) I have to type `nosetests-3.4` in the `<sphinx_repo>/prototype` directory.
--- prototype/bootstrap.py
+++ prototype/bootstrap.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*- {{{
-# vim: set fenc=utf-8 ft=python ff=unix noet sts=0 sw=4 ts=4 :
 # APBS -- Adaptive Poisson-Boltzmann Solver
 #
 #  Nathan A. Baker (nathan.baker@pnnl.gov)
@@ -8,7 +7,7 @@
 #
 #  Additional contributing authors listed in the code documentation.
 #
-# Copyright (c) 2010-2015 Battelle Memorial Institute. Developed at the
+# Copyright (c) 2010-2016 Battelle Memorial Institute. Developed at the
 # Pacific Northwest National Laboratory, operated by Battelle Memorial
 # Institute, Pacific Northwest Division for the U.S. Department of Energy.
 #
@@ -39,15 +38,178 @@
 # THE POSSIBILITY OF SUCH DAMAGE.
 #}}}
 
+import errno
+import os
+import subprocess
 import sys
-
-from sphinx.bootstrap import *
 
 __author__ = 'Keith T. Star <keith@pnnl.gov>'
 
-def main():
-	# Generate the PDBx/mmCIF JSON Schema
-	generate_types()
+_WINDOWS = sys.platform.startswith('win')
+_ENV = 'env'
+_PACKAGES = 'packages.txt'
 
+
+def bootstrap(dest=_ENV, prompt='(sphinx)'):
+    '''Download and install Virtualenv to setup our Python environment.
+
+    The virtual environtment well be setup in dest.
+    '''
+
+    import contextlib
+    import re
+    import shutil
+    import tarfile
+    import tempfile
+    from urllib.request import urlopen
+
+    _DOAP_URL = "https://pypi.python.org/pypi?:action=doap&name=virtualenv"
+    _VENV_URL = "https://pypi.python.org/packages/source/v/virtualenv/virtualenv-{}.tar.gz"
+
+    class EnvBuilder:
+        def __init__(self, version=None, prompt=None):
+            self.version = version
+            self.prompt = prompt
+            self.env_exe = None
+
+        def _fetch(self, url):
+            '''Open URL and return response, or quit'''
+            sys.stdout.write("Fetching {}...\n".format(url))
+
+            response = urlopen(url)
+            if response.getcode() != 200:
+                sys.stderr.write('Download failed!  Server response is {} {}.\n'.format(
+                    response.code, response.msg))
+                sys.exit(1)
+
+            return response
+
+        def get_version(self):
+            '''Return the latest version from the Virtualenv DOAP record.'''
+            with contextlib.closing(self._fetch(_DOAP_URL)) as response:
+                doap_xml = response.read()
+
+                self.version = re.search(b'<revision>([^<]*)</revision>',
+                                         doap_xml).group(1).decode('utf-8')
+
+            return self.version
+
+        def download(self, directory):
+            '''Download the Virtualenv tarball'''
+            if self.version is None:
+                self.get_version()
+
+            url = _VENV_URL.format(self.version)
+            sys.stdout.write("Downloading Virtualenv {}\n".format(self.version))
+            tarball = os.path.join(directory, 'virtualenv.tar.gz')
+            with contextlib.closing(self._fetch(url)) as response:
+                with open(tarball, 'wb') as file:
+                    shutil.copyfileobj(response, file)
+
+            with contextlib.closing(tarfile.open(tarball, 'r|gz')) as archive:
+                archive.extractall(directory)
+
+        def create(self, directory):
+            '''Create a virtual environtment.'''
+            tmpdir = tempfile.mkdtemp()
+            try:
+                self.download(tmpdir)
+                args = [sys.executable]
+                args.append(os.path.join(tmpdir, 'virtualenv-{}'.format(
+                    self.version), 'virtualenv.py'))
+                if self.prompt:
+                    args.extend(['--prompt', prompt])
+
+                args.append(directory)
+                subprocess.check_call(args)
+
+                if _WINDOWS:
+                    self.env_exe = os.path.join(directory, 'Scripts', 'python.exe')
+                else:
+                    self.env_exe = os.path.join(directory, 'bin', 'python')
+
+                assert(os.path.exists(self.env_exe))
+                                
+            finally:
+                shutil.rmtree(tmpdir, ignore_errors=True)
+                            
+
+    sys.stdout.write("Creating Python virtual environment...\n")
+    builder = EnvBuilder(prompt=prompt)
+    builder.create(dest)
+        
+    return builder.env_exe
+
+
+def run_pip():
+    '''Call pip in the virtual environment to install packages.'''
+    cmd = ['pip', 'install']
+    cmd[:0] = [sys.executable, '-m']
+    subprocess.check_called(cmd)
+
+
+def install_packages():
+    '''Install and or update dependencies from packages.txt.'''
+    path = os.path.dirname(__file__) or '.'
+    packages = os.path.join(path, _PACKAGES)
+    if os.path.exists(packages):
+        cmd = [sys.executable, '-m', 'pip', 'install', '-r', packages]
+        subprocess.check_call(cmd)
+
+
+def main(argv=sys.argv):
+    # Refuse to run as root
+    if not getattr(os, 'getuid', lambda: -1)():
+        sys.stderr.write("{}: error; please don't run as root!\n".format(
+            os.path.basename(argv[0])))
+        sys.exit(7)
+
+    python = os.path.join('$VIRTUAL_ENV', 'Scripts' if _WINDOWS
+                          else 'bin', 'python')
+
+    if _WINDOWS:
+        python += '.exe'
+
+    # We need Python 3.4 for asyncio support
+    if sys.version_info.major != 3 and sys.version_info.minor >= 4:
+        sys.stderr.write("{}: error; Python > 3.4 is required.\n".format(
+            os.path.basename(argv[0])))
+        sys.exit(6)
+
+    # Install Virtualenv and Sphinx dependencies
+    if hasattr(sys, 'real_prefix'):
+        # script called from virtual environment, so install/update and bootstrap modules
+        install_packages()
+
+        from sphinx.bootstrap import generate_types
+        # Generate the PDBx/mmCIF JSON Schema
+        generate_types()
+
+    else:
+        # script called from system python -- bootstrap
+        try:
+            # Don't install if our environment is already setup
+            if os.listdir(_ENV):
+                sys.stderr.write("{}: error; Virtualenv is already setup.\n".format(
+                    os.path.basename(argv[0])))
+                sys.exit(5)
+
+        except OSError as exc:
+            if exc.errno != errno.ENOENT:
+                raise
+            
+        env_exe = bootstrap()
+
+        # Now do stage 2...
+        # TODO: On OSX this doesn't appear to give the correct path to the Python
+        # executable, and pip doesn't install packages into the local environment.
+        args = [env_exe, __file__]
+        subprocess.check_call(args)
+
+                
+                
 if __name__ == '__main__':
-	sys.exit(main())
+	try:
+		main()
+	except KeyboardInterrupt:
+		pass
