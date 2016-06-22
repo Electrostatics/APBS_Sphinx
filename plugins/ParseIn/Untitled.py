fe6aa28542dd661d9f9a5c6611b4c01cce92f482
Tartakovsky <tart748@we27398.pnl.gov>
2016-06-22 09:51:06 -0700

    Adding ParseIN dir and other stuff

--- plugins/ParseIn/Untitled.py
+++
@@ -1,5 +0,0 @@
-Python 3.5.1 (v3.5.1:37a07cee5969, Dec  5 2015, 21:12:44) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
