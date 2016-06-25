607c2cd0411cc06d2da6becea421fa9adcc3bb64
Keith T. Star <keith@pnnl.gov>
2016-06-24 18:47:08 -0700

    This commit is the first step in a wholesale integration of PDB2PQR into Sphinx.  Note that the extensions are not enabled.  Also, the only options available to the plugin are the PDB file, the force field to use, and the output file.  This does not work with the pipeline.  It reads the PDB file and writes the PQR file.  It's very rough, but it's an awesome first step. Makes progress on issue #11.

---
+++ plugins/PDB2PQR/plugin.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*- {{{
+# vim: set fenc=utf-8 ft=python ff=unix sw=4 ts=4 sts=4 et:
+# APBS -- Adaptive Poisson-Boltzmann Solver
+#
+#  Nathan A. Baker (nathan.baker@pnnl.gov)
+#  Pacific Northwest National Laboratory
+#
+#  Additional contributing authors listed in the code documentation.
+#
+# Copyright (c) 2010-2016 Battelle Memorial Institute. Developed at the
+# Pacific Northwest National Laboratory, operated by Battelle Memorial
+# Institute, Pacific Northwest Division for the U.S. Department of Energy.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# Neither the name of the developer nor the names of its contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+#}}}
+
+import asyncio
+import logging
+
+from sphinx.plugin import BasePlugin
+from .main import mainCommand
+
+__author__ = 'Keith T. Star <keith@pnnl.gov>'
+
+_log = logging.getLogger()
+
+class PDB2PQR(BasePlugin):
+    '''Stuff PDB2PQR into Sphnx
+    '''
+    def __init__(self, pdb_name, output, ff, **kwargs):
+        super().__init__(**kwargs)
+
+        self._pdb = pdb_name
+        self._out = output
+        self._ff = ff
+
+        _log.info("PDB2PQR plug-in initialized.")
+
+
+    @classmethod
+    def script_name(cls):
+        return "pdb2pqr"
+
+
+    @classmethod
+    def sinks(cls):
+        return ['text']
+
+
+    @classmethod
+    def sources(cls):
+        return ['text']
+
+
+    @asyncio.coroutine
+    def run(self):
+        mainCommand(['pdb2pqr-sphinx', '--ff={}'.format(self._ff), self._pdb, self._out])
+        yield from self.done()
+
+
+    def xform_data(self, data, to_type):
+        return data
